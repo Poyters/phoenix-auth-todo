b3cd17d2f162472d1e0b5372f790a66814eaea6e
Rafał Kostecki <edward-12@o2.pl>
2024-04-04 22:55:20 +0200

    feat: add redirect to tasks route

--- lib/todo_app_web/controllers/page_controller.ex
+++ lib/todo_app_web/controllers/page_controller.ex
@@ -1,6 +1,12 @@
 defmodule TodoAppWeb.PageController do
   use TodoAppWeb, :controller
 
+  def index(conn, _params) do
+    conn
+    |> put_status(:moved_permanently)
+    |> redirect(to: ~p"/tasks")
+  end
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
